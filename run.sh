98625cea0e2f5bbeb3451a2fe5b9013ec3ec497f
NischayDiwan <cs5200433@iitd.ac.in>
2023-04-25 22:54:34 +0530

    adjustment

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-python gen-input.py -n 8000 -m 8 -z 100000 -o input1m
-python gen-input.py -n 8000 -m 8 -z 100000 -o input2m
-# python gen-output.py -f input1 input2 -e 2 -o output1
+python gen-input.py -n 8000 -m 8 -z 10000 -o input1
+python gen-input.py -n 8000 -m 8 -z 10000 -o input2
+python gen-output.py -f input1 input2 -e 2 -o output1
 # python gen-output.py -f input2 input1 -e 2 -o output2
 # python checker.py -f output1 output1 -e 4
 # python checker.py -f output1 output2 -e 4
--- testrun.sh
+++ testrun.sh
@@ -1,3 +1,3 @@
 make 
-time ./exec input1m input2m outFile.bin
-# python checker.py -f output1 outFile.bin -e 4
+time ./exec input1 input2 outFile.bin
+python checker.py -f output1 outFile.bin -e 4

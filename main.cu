5bc5ef294dbe04d25ba5b4955b40fb40c04a7da6
NischayDiwan <cs5200433@iitd.ac.in>
2023-04-28 20:28:18 +0530

    submit ready

--- main.cu
+++ main.cu
@@ -4,10 +4,6 @@
 #include <chrono>
 
 #define MAX_VAL 4294967295
-#define BLOCK_SIZE1 16
-#define BLOCK_SIZE2 64
-#define BLOCK_SIZEGPU blockDim.x
-#define timer 0
 
 using namespace std;
 void printvec(vector<uint> &a,ofstream &outstr, int m){
@@ -35,25 +31,8 @@ bool isZero(uint *v, int l){
 	return flg;
 }
 
-__device__
-int binASearch(int *v, int l, int p1, int p2){
-	// binary search in array
-	int s = 0;
-	int e = l;
-	while(s <= e){
-		int m = s + (e-s) / 2;
-		if(v[3*m] == p1 && v[3*m+1] == p2)
-			return v[3*m +2];
-		if((v[3*m] < p1) || (v[3*m] == p1 && v[3*m+1] < p2))
-			s = m + 1;
-		else
-			e = m - 1;
-	}
-	return -1;
-}
-
 __global__
-void matMulGPU(uint *a, uint *b, uint *c, int *ka, int *kb, int *rof, int *col, int m, int n, int k1, int k2){
+void matMulGPU(uint *a, uint *b, uint *c, int *ka, int *kb, int *rof, int *col, int* row, int *cof, int m, int n, int k1, int k2){
 	extern __shared__ uint dab[];
 	int bid = blockIdx.x;
 	int tid = threadIdx.x;
@@ -61,48 +40,45 @@ void matMulGPU(uint *a, uint *b, uint *c, int *ka, int *kb, int *rof, int *col, 
 	int i = bid / nm;
 	int k = bid % nm;
 	uint64_t temp = 0;
-	// if(tid == 0 && bid == 0){
-	// for (int it = 0; it < k1; it++)
-	// {
-	// 	printf("%d %d %d\n",ka[3*it],ka[3*it+1],ka[3*it+2]);
-	// }
-	// printf("-------------------\n");
-	// for (int it = 0; it < k2; it++)
-	// {
-	// 	printf("%d %d %d\n",kb[3*it],kb[3*it+1],kb[3*it+2]);
-	// }
-	// printf("-------------------\n");
-	// }
-	for (int j = rof[i]; j < rof[i+1]; j++){
-		// int id1 = binASearch(ka,k1,i,j);
-		int id1 = ka[j];
-		int cl = col[j];
-		int id2 = binASearch(kb,k2,cl,k);
-		// if(tid == 0 && bid == 0)
+	int j1 = rof[i];
+	int j2 = cof[k];
+	while (j1 < rof[i+1] && j2 < cof[k+1]){
+		int id1 = ka[j1];
+		int id2 = kb[j2];
+		int cl = col[j1];
+		int rw = row[j2];
+		int flag = 0;
+		if(cl == rw){
+			flag = 1;
+			j1++;
+			j2++;
+		}
+		else if(cl < rw)
+			j1++;
+		else
+			j2++;
+		// if(tid == 0 && bid == 9)
 		// 	printf("%d %d : %d %d : %d %d\n",i,cl,cl,k,id1,id2);
-		if(!(id2 == -1)){
+		if(flag){
+			__syncthreads();
 			dab[tid] = (uint)a[tid + id1*m*m];
 			dab[tid + m*m] = (uint)b[tid + id2*m*m];
 			__syncthreads();
-			
 			int ii = tid/m;
 			int jj = tid%m;
 			for (int kk = 0; kk < m; ++kk)
 			{
 				temp = temp + (uint64_t)(dab[ii*m + kk] * dab[kk*m + jj + m*m]);
 			}
-			__syncthreads();
 		}
-		__syncthreads();
 	}
-	__syncthreads();
 	c[tid + i*m*n + k*m*m] = min(temp,MAX_VAL);
 }
 
 void matMul(vector<array<int,3>> &mp1, vector<uint> &blksA, vector<array<int,3>> &mp2, vector<uint> &blksB,  long long n, long long m, vector<uint> &blksC){
 	long long nm = n/m;
 	// sending data to GPU
-	int streamSize = 6;
+	int streamSize = 2;
 	cudaError_t err;
 	cudaStream_t stream[streamSize];
 	for(int i = 0;i<streamSize ;i++){
@@ -111,8 +87,8 @@ void matMul(vector<array<int,3>> &mp1, vector<uint> &blksA, vector<array<int,3>>
 	size_t size = sizeof(uint);
 	size_t size2 = sizeof(int);
 	size_t size3 = sizeof(uint) * n * n;
-	uint *a = blksA.data(), *b = blksB.data(), *da, *db;
-	uint *c = blksC.data(), *dc;
+	uint *a = &blksA[0], *b = &blksB[0], *da, *db;
+	uint *c = &blksC[0], *dc;
 	cudaMalloc(&da,size*blksA.size());
 	cudaMalloc(&db,size*blksB.size());
 	cudaMalloc(&dc,size3);
@@ -120,15 +96,6 @@ void matMul(vector<array<int,3>> &mp1, vector<uint> &blksA, vector<array<int,3>>
 	cudaMemset(dc,0,size3);
 	cudaMemcpyAsync(da,a,(size_t)size*(size_t)blksA.size(),cudaMemcpyHostToDevice,stream[0]);
 	cudaMemcpyAsync(db,b,(size_t)size*(size_t)blksB.size(),cudaMemcpyHostToDevice,stream[1]);
-	// int *ka;
-	// cudaMalloc(&ka,(size_t)3*size2*(size_t)mp1.size());
-	// cudaMemcpyAsync(ka,mp1.data(),(size_t)3*size2*(size_t)mp1.size(),cudaMemcpyHostToDevice,stream[2]);
-	int *kb;
-	cudaMalloc(&kb,(size_t)3*size2*(size_t)mp2.size());
-	cudaMemcpyAsync(kb,mp2.data(),(size_t)3*size2*(size_t)mp2.size(),cudaMemcpyHostToDevice,stream[3]);
-	// err = cudaGetLastError();
-	// if (err != cudaSuccess) 
-	//     printf("Error: %s\n", cudaGetErrorString(err));
 
 	// converting to CSR
 	vector<int> valV;
@@ -151,32 +118,53 @@ void matMul(vector<array<int,3>> &mp1, vector<uint> &blksA, vector<array<int,3>>
 	for(int j = rowno;j<nm;j++){
 		rofV.push_back(blksA.size()/m/m);
 	}
+	vector<int> valV2;
+	vector<int> cofV;
+	vector<int> rowV;
+	offset = 0;
+	int colno = 0;
+	cofV.push_back(offset);
+	for(int i = 0; i <mp2.size() ; i++){
+		rowV.push_back(mp2[i][0]);
+		valV2.push_back(mp2[i][2]);
+		if(mp2[i][1] > colno){
+			for(int cc = 0;cc<(mp2[i][1]-colno);cc++){
+				cofV.push_back(offset);
+			}
+			colno = mp2[i][1];
+		}
+		offset+=1;
+	}
+	for(int j = colno;j<nm;j++){
+		cofV.push_back(blksB.size()/m/m);
+	}
+	std::cout << "CSR converted\n";
 
-	// std::cout << "CSR converted\n";
 	int *ka;
 	cudaMalloc(&ka,(size_t)size2*(size_t)mp1.size());
-	cudaMemcpyAsync(ka,valV.data(),(size_t)size2*(size_t)mp1.size(),cudaMemcpyHostToDevice,stream[2]);
-	int *rof, *col;
+	cudaMemcpyAsync(ka,valV.data(),(size_t)size2*(size_t)mp1.size(),cudaMemcpyHostToDevice,stream[0]);
+	int *kb;
+	cudaMalloc(&kb,(size_t)size2*(size_t)mp2.size());
+	cudaMemcpyAsync(kb,valV2.data(),(size_t)size2*(size_t)mp2.size(),cudaMemcpyHostToDevice,stream[1]);
+	int *rof, *col, *cof, *row;
 	cudaMalloc(&rof,(size_t)rofV.size()*size2);
 	cudaMalloc(&col,(size_t)colV.size()*size2);
-	cudaMemcpyAsync(rof,&rofV[0],(size_t)rofV.size()*size2,cudaMemcpyHostToDevice,stream[4]);
-	cudaMemcpyAsync(col,&colV[0],(size_t)colV.size()*size2,cudaMemcpyHostToDevice,stream[5]);
+	cudaMalloc(&cof,(size_t)cofV.size()*size2);
+	cudaMalloc(&row,(size_t)rowV.size()*size2);
+	cudaMemcpyAsync(rof,&rofV[0],(size_t)rofV.size()*size2,cudaMemcpyHostToDevice,stream[0]);
+	cudaMemcpyAsync(col,&colV[0],(size_t)colV.size()*size2,cudaMemcpyHostToDevice,stream[1]);
+	cudaMemcpyAsync(cof,&cofV[0],(size_t)cofV.size()*size2,cudaMemcpyHostToDevice,stream[0]);
+	cudaMemcpyAsync(row,&rowV[0],(size_t)rowV.size()*size2,cudaMemcpyHostToDevice,stream[1]);
 
 	int stride = (int)(nm*nm);
-	if(timer){
-		cudaDeviceSynchronize();
-		chrono::time_point<std::chrono::system_clock> startg = std::chrono::system_clock::now();
-		matMulGPU<<<stride,m*m,2*size*m*m,0>>>(da,db,dc,ka,kb,rof,col,m,n,mp1.size(),mp2.size()); // i X k
-		cudaDeviceSynchronize();
-		chrono::time_point<std::chrono::system_clock> endg = std::chrono::system_clock::now();
-		chrono::duration<double> elapsed_secondsg = endg-startg;
-		std::cout << "gpu multiplication time: " << elapsed_secondsg.count() << "s\n";
-	}else{
-		cudaDeviceSynchronize();
-		matMulGPU<<<stride,m*m,2*size*m*m,0>>>(da,db,dc,ka,kb,rof,col,m,n,mp1.size(),mp2.size()); // i X k
-	}
-
+	// chrono::time_point<std::chrono::system_clock> startg = std::chrono::system_clock::now();
 	cudaDeviceSynchronize();
+	matMulGPU<<<stride,m*m,2*size*m*m,0>>>(da,db,dc,ka,kb,rof,col,row,cof,m,n,mp1.size(),mp2.size()); // i X k
+	// cudaDeviceSynchronize();
+	// chrono::time_point<std::chrono::system_clock> endg = std::chrono::system_clock::now();
+	// chrono::duration<double> elapsed_secondsg = endg-startg;
+	// std::cout << "gpu multiplication time: " << elapsed_secondsg.count() << "s\n";
+
 	err = cudaMemcpy((void *)c,(void *)dc,size3,cudaMemcpyDeviceToHost);
 	if (err != cudaSuccess) 
 	    printf("Error: %s\n", cudaGetErrorString(err));
@@ -193,6 +181,16 @@ void matMul(vector<array<int,3>> &mp1, vector<uint> &blksA, vector<array<int,3>>
 		cudaStreamDestroy(stream[i]);
 	}
 }
+
+bool compmy(const array<int,3> &a, const array<int,3> &b){
+	if(a[1] == b[1]){
+		if(a[0] == b[0]){
+			return a[2] < b[2];
+		}
+		return a[0] < b[0];
+	}
+	return a[1] < b[1];
+};
 
 int main(int argc, char const *argv[])
 {
@@ -289,10 +287,10 @@ int main(int argc, char const *argv[])
 		valb.insert(valb.end(),vt.begin(),vt.end());
     }
 	fileB.close();
-    cout << "Input reading done, n: " << n << " m: " << m <<  " k values: " << keysa.size() << " and " << keysb.size() << endl;
+    cout << "Input reading done, n: " << n << ", m: " << m <<  ", k-values: " << keysa.size() << " and " << keysb.size() << endl;
 	
 	sort(keysa.begin(), keysa.end());
-	sort(keysb.begin(), keysb.end());
+	sort(keysb.begin(), keysb.end(), compmy);
 	
 	assert(keysa.size() == vala.size()/m/m);
 	assert(keysb.size() == valb.size()/m/m);
--- testrun.sh
+++ testrun.sh
@@ -1,6 +1,12 @@
-make 
-time ./exec input1 input2 outFile.bin
-python checker.py -f output1 outFile.bin -e 4
-# time ./exec input1m input2m outFile.bin
-# time ./exec ../input1l ../input2l outFile.bin
+make
+for i in 10 50 100 500 1000 5000 10000
+do
+    python gen-input.py -n 8192 -m 4 -z ${i} -o input1
+    python gen-input.py -n 8192 -m 4 -z 7889 -o input2
+    python gen-output.py -f input1 input2 -e 2 -o output1
+    time ./exec input1 input2 outFile.bin
+    python checker.py -f output1 outFile.bin -e 4
+done
+# time ./exec input1v input2v outFile.bin
+# python checker.py -f output1v outFile.bin -e 4
 # cuda-gdb --args ./exec input1 input2 outFile.bin
